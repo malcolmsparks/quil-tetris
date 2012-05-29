dd7a91a83d15c77c6788e0c49ce38c74d2849cbb
Malcolm Sparks <malcolm@congreve.com>
2012-05-29 22:26:06 +0100

    checkpoint

--- src/tetris/core.clj
+++ src/tetris/core.clj
@@ -11,12 +11,11 @@
 ;(defn rotate90 [model] (map (fn [[x y]] [-y x]) model))
 
 (defn block [x y]
-  (rect (* block-width x) (- game-height-pixels (* block-width y)) block-width block-width)
-  )
+  (rect (* block-width x) (- game-height-pixels (* block-width y)) block-width block-width))
 
 (def rows (int (/ game-height-pixels block-width)))
 
-(def shapes
+(def tetriminos
   {:i [:maroon [0 0] [1 0] [2 0] [3 0]]
    :j [:white [0 0] [1 0] [2 0] [2 1]]
    :l [:magenta [0 0] [0 1] [1 0] [2 0]]
@@ -25,9 +24,9 @@
    :t [:brown [0 0] [1 0] [2 0] [1 1]]
    :z [:cyan [0 0] [0 1] [1 1] [2 1]]})
 
-(defn draw-tetrimino [{piece :piece x0 :x y0 :y}]
+(defn draw-tetrimino [{tetrimino :tetrimino x0 :x y0 :y}]
   (dorun
-   (let [[color & coords] (shapes piece)]
+   (let [[color & coords] (tetriminos tetrimino)]
      (apply fill (get {:maroon [128 0 0]
                       :white [255 255 255]
                       :magenta [255 0 255]
@@ -40,12 +39,11 @@
        (block (+ x x0) (+ y y0))))))
 
   
-(def current (ref {:piece :o :x 7 :y rows :rotation 0}))
+(def current (ref {:tetrimino :o :x 7 :y rows :rotation 0}))
 
 (defn new-tetrimino []
-  (alter stopped-tetriminos conj current)
   (alter current assoc
-         :piece (rand-nth (keys shapes))
+         :tetrimino (rand-nth (keys shapes))
          :x (rand-int (+ 2 (- cols 4)))
          :y rows))
 
@@ -57,25 +55,21 @@
   
   (fill 0 0 0)
   (rect 0 0 game-width-pixels game-height-pixels)
-  (draw-tetrimino @current)
-  )
-
+  (draw-tetrimino @current))
 
 (defn mouse-moved []
   (let [x (max 0 (min (- game-width-pixels 20) (mouse-x))) y (mouse-y)]
-    (dosync (alter current assoc :x (/ x block-width)))
-    ))
+    (dosync (alter current assoc :x (/ x block-width)))))
 
 (defn rotate-block []
   (dosync (alter current update-in [:rotation] inc))
   )
 
 (defn setup []
-  (dosync (ref-set current {:piece :o :x 7 :y 2}))
+  (dosync (ref-set current {:tetrimino :o :x 7 :y rows}))
   (smooth)
   (background 0 0 0)
-  (frame-rate 3)
-  )
+  (frame-rate 3))
 
 (defsketch gen-art-1
   :title "Tetris"
